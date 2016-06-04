3043be360cdb345108c157a1cb569b3d735854cf
elevatedmdg <admin@elevatedmdg.com>
2016-06-04 04:15:54 -0700

    config browser sync and added emdg custom functions

---
+++ site/web/app/themes/sage/lib/emdg.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * elevated Media Design Group
+ *
+ * Custom Site Utilities and Settings
+ *
+ * @link http://www.elevatedMDG.com
+ *
+ */
+
+ require_once locate_template('/lib/emdg/emdg-custom.php');					// eMDG Custom - Custom Functions
