ec3ca64ac04d378679732ac90a9181acdeabeaaf
elevatedmdg <admin@elevatedmdg.com>
2016-06-04 04:23:59 -0700

    adding emdg custom functions

--- site/web/app/themes/sage/lib/emdg.php
+++ site/web/app/themes/sage/lib/emdg.php
@@ -9,4 +9,22 @@
  *
  */
 
- require_once locate_template('/lib/emdg/emdg-custom.php');					// eMDG Custom - Custom Functions
+ // default load all additional templates as neccessary
+ // require_once locate_template('/lib/emdg/emdg-post-formats.php');			  // eMDG Custom - Post Formatsd
+ // require_once locate_template('/lib/emdg/emdg-staff.php');                  // eMDG Custom - CPT Staff
+ require_once locate_template('/lib/emdg/emdg-testimonials.php');			      // eMDG Custom - CPT Testimonials
+ // require_once locate_template('/lib/emdg/emdg-portfolio.php');				      // eMDG Custom - Portfolio
+ require_once locate_template('/lib/emdg/emdg-metaboxes.php');				      // eMDG Custom - Enable Meta Boxes
+ require_once locate_template('/lib/emdg/emdg-metaboxes-post-formats.php');	// eMDG Custom - Enable Post Format Meta Boxes
+ // require_once locate_template('/lib/emdg/emdg-shortcodes.php');				      // eMDG Custom - Shortcodes
+ // require_once locate_template('/lib/emdg/emdg-breadcrumbs.php');				    // eMDG Custom - Breadcrumbs
+ // require_once locate_template('/lib/emdg/emdg-sidebar.php');					    // eMDG Custom - Sidebar Control
+ // require_once locate_template('/lib/emdg/emdg-gforms-2.php');				    // eMDG Custom - Gravity Forms Fixes
+ // require_once locate_template('/lib/emdg/emdg-gforms-styles.php');		    // eMDG Custom - Gravity Forms Fixes
+ // require_once locate_template('/lib/emdg/emdg-fonts.php');					        // eMDG Custom - Font Loader
+ // require_once locate_template('/lib/emdg/emdg-redux.php');				        // eMDG Custom - Redux
+ require_once locate_template('/lib/emdg/emdg-custom.php');					        // eMDG Custom - Custom Functions
+ // require_once locate_template('/lib/emdg/emdg-menus.php');					        // eMDG Custom - Menu Loader
+ // require_once locate_template('/lib/emdg/emdg-theme-menu.php');				  // eMDG Custom - Theme Menu
+ // require_once locate_template('/lib/emdg/emdg-class-tgm-plugin-activation.php');		// eMDG Custom - TGM Class Activation
+ // require_once locate_template('/lib/emdg/emdg-tgm-req-plugins.php');			// eMDG Custom - Plugin Loader List
---
+++ site/web/app/themes/sage/lib/emdg/emdg-testimonials.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * elevated Media Design Group
+ *
+ * Testimonials Template
+ *
+ * @link http://www.elevatedMDG.com
+ *
+ */
+
+// add custom post type
+add_action( 'init', 'create_post_type_testimonials' );
+function create_post_type_testimonials() {
+	register_post_type( 'testimonials',
+		array(
+			'labels' => array(
+				'name' => __( 'Testimonials' ),
+				'singular_name' => __( 'Testimonial' ),
+				'add_new' => __( 'Add New' ),
+				'add_new_item' => __( 'Add New Testimonial' ),
+				'edit' => __( 'Edit' ),
+				'edit_item' => __( 'Edit Testimonial' ),
+				'new_item' => __( 'New Testimonial' ),
+				'view' => __( 'View Testimonial' ),
+				'view_item' => __( 'View Testimonial' ),
+				'search_items' => __( 'Search Testimonials' ),
+				'not_found' => __( 'No Testimonials Found' ),
+				'not_found_in_trash' => __( 'No Testimonials Found in Trash' ),
+				'parent' => __( 'Parent' )
+			),
+		'public' => true,
+		'menu_position' => 5,
+        	'show_ui' => true,
+        	'capability_type' => 'post',
+        	'hierarchical' => false,
+		'supports' => array('title'),
+		'rewrite' => array('slug' => 'testimonial', 'with_front' => false),
+		'has_archive' => 'testimonial-archives',
+		)
+	);
+}
+
+//add associated meta boxes
+$meta_box['testimonials'] = array(
+	'id' => 'testimonials-written',
+	'title' => 'Testimonials',
+	'context' => 'normal',
+	'priority' => 'high',
+	'fields' => array(
+		array(
+			'name' => 'Author',
+			'desc' => '',
+			'id' => 'testimonial_author',
+			'type' => 'text',
+			'default' => ''
+			),
+		array(
+			'name' => 'Testimonial',
+			'desc' => '',
+			'id' => 'testimonial_actual',
+			'type' => 'textarea',
+			'default' => ''
+			),
+		array(
+			'name' => 'Star Rating',
+			'desc' => '',
+			'id' => 'testimonial_rating',
+			'type' => 'select',
+			'options' => array('5', '4', '3', '2', '1')
+			),
+		)
+	);
