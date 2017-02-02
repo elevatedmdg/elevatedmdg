ec157add9b0e8d936f4c359a27767096c60aa917
Elevated Media Design Group <elevatedmdg@users.noreply.github.com>
2017-02-02 12:57:21 -0800

    Testing CodeHub iOS workflow while updating search.php with better suggestion.

--- site/web/app/themes/sage/search.php
+++ site/web/app/themes/sage/search.php
@@ -2,7 +2,7 @@
 
 <?php if (!have_posts()) : ?>
   <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
+    <?php _e('Sorry, no results were found. Please check your spelling or search again using a different phrase or something slightly less specific.', 'sage'); ?>
   </div>
   <?php get_search_form(); ?>
 <?php endif; ?>
